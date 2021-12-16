ba544d3eea4285fd5591a93420224cd52b8847a7
Alberto Garcia <josealbertogarcia453@gmail.com>
2021-12-15 20:10:57 -0600

    Simple factory

---
+++ src/factory/ChicagoPizzaStore.java
@@ -0,0 +1,20 @@
+package factory;
+
+public class ChicagoPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        if (type.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        }
+        /*else if (type.equals("veggie")) {
+            return new NYStyleVeggiePizza();
+        }
+        else if (type.equals("clam")) {
+            return new NYStyleClamPizza();
+        }
+        else if (type.equals("pepperoni")) {
+            return new NYStylePepperoniPizza();
+        }*/
+        else return null;
+    }
+}
